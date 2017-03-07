6386bff438ef677fb6b961c69f5529a32b617c58
aronysidoro <pyaaron@gmail.com>
2017-03-07 07:14:26 -0800

    pre-removal of BillingTwo

--- tests/unit/models/account-test.js
+++ tests/unit/models/account-test.js
@@ -9,7 +9,7 @@ moduleForModel('account', 'aaron move method', {
 
 const MIN_BALANCE = 0;
 
-function Account(obj={}) {
+function Account(obj = {}) {
   const x = Object.assign({
     balance: obj.balance ? obj.balance : 0,
   }, obj);
@@ -22,18 +22,76 @@ function Account(obj={}) {
 }
 
 function Billing() {
-  return 5;
+  this.rechargeAccount = function(account) {
+    if (account.shouldRecharge()) {
+      // process payment
+      account.balance += 10;
+    }
+  };
 }
 
 test('Account defaults', function(assert) {
-  const account = Account();
+  const account = new Account();
   assert.equal(account.balance, 0);
   assert.ok(account.shouldRecharge());
 });
 
 test('Billing will process account', function(assert) {
-  let account = Account();
+  let account = new Account();
   assert.equal(account.balance, 0);
 
-  // const billing = n 
+  const billing = new Billing();
+  billing.rechargeAccount(account);
+  assert.equal(account.balance, 10);
+});
+
+// better
+
+function BillingTwo() {
+  this.rechargeAccount = function(account) {
+    if (account.shouldRecharge(account)) {
+      // process payment
+      account.balance += 10;
+    }
+  };
+  this.shouldRecharge = function(account) {
+    return account.balance <= MIN_BALANCE;
+  };
+}
+
+test('Billing will process account', function(assert) {
+  let account = new Account();
+  assert.equal(account.balance, 0);
+
+  const billing = new BillingTwo();
+  assert.equal(billing.shouldRecharge(account), true);
+
+  billing.rechargeAccount(account);
+  assert.equal(account.balance, 10);
+});
+
+test('Billing shouldRecharge', function(assert) {
+  const billing = new BillingTwo();
+
+  let account = new Account();
+  assert.equal(account.balance <= MIN_BALANCE, true);
+  assert.equal(billing.shouldRecharge(account), true);
+
+  account = new Account({balance: 1});
+  assert.equal(account.balance <= MIN_BALANCE, false);
+  assert.equal(billing.shouldRecharge(account), false);
+});
+
+// split temp variable
+
+test('Billing shouldRecharge', function(assert) {
+  const billing = new BillingTwo();
+
+  const accountOne = new Account();
+  assert.equal(accountOne.balance <= MIN_BALANCE, true);
+  assert.equal(billing.shouldRecharge(accountOne), true);
+
+  const accountTwo = new Account({balance: 1});
+  assert.equal(accountTwo.balance <= MIN_BALANCE, false);
+  assert.equal(billing.shouldRecharge(accountTwo), false);
 });
